7537ca73a939e207499ad2f975c3ba742952ab72
5solbin <dhrhfqls@naver.com>
2024-11-25 17:49:54 +0900

    yml 설정파일, 시큐리티 설정

--- src/main/java/savenow/backend/config/SecurityConfig.java
+++ src/main/java/savenow/backend/config/SecurityConfig.java
@@ -1,25 +1,80 @@
 package savenow.backend.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final Logger log = LoggerFactory.getLogger(getClass()); // 로그 확인용
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        log.debug("디버그 : BcryptPasswordEncoder 빈 등록됨");
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.debug("디버그 : filterChain 빈 등록됨");
         http
-                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (필요에 따라 활성화 가능)
-                .cors(Customizer.withDefaults()) // 기본 CORS 설정 활성화
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보호 비활성화 (필요에 따라 활성화 가능)
+                .httpBasic(AbstractHttpConfigurer::disable)// http basic 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 리액트로 요청 예정
+                .cors(cors -> cors.configurationSource(configurationSource())) // 자바스크립트 요청 허용
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // Iframe 허용 안함
+
+                // JWT 서버로 만들어서 세션 사용 안함 jsessionId 서버쪽 관리 X
+                .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/test").permitAll() // 특정 엔드포인트 인증 없이 접근 가능
+                        .requestMatchers(
+                                AntPathRequestMatcher.antMatcher("/api/join")
+                        ).permitAll()
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
-                )
-                .httpBasic(Customizer.withDefaults()); // HTTP Basic 인증 활성화
+                );
 
         return http.build();
     }
+
+    public CorsConfigurationSource configurationSource() {
+        log.debug("디버그 : configurationSource cors 설정이 SecurityFilterChain에 등록됨");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트 앤드 IP만 허용 react)
+        configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    // h2 로컬 환경에서도 테스트 가능
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled",havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
+    }
+
 }
+
