9ec74326075fca74d438a43f069a8c13744e58cc
5solbin <dhrhfqls@naver.com>
2024-11-25 23:24:45 +0900

    validation 수정

--- src/main/java/savenow/backend/dto/user/UserReqDto.java
+++ src/main/java/savenow/backend/dto/user/UserReqDto.java
@@ -15,18 +15,19 @@ public class UserReqDto {
     public static class JoinReqDto {
         // 회원 이름 : 한글, 영어 숫자 가능, 12자 이내
         @NotBlank
-        @Pattern(regexp = "^[a-zA-Z가-힣0-9]{2,12}$" , message = "한글/영문/숫자 2~12자 이내로 작성해 주세요")
+        @Pattern(regexp = "^[a-zA-Z가-힣0-9]{2,20}$" , message = "한글/영문/숫자 2~20자 이내로 작성해 주세요")
         private String username;
 
         @NotBlank
-        @Pattern(regexp = "^[a-zA-z0-9]{4,12}$", message = "한글/영문/숫자 4~12자 이내로 작성해주세요")
+        @Pattern(regexp = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$",
+        message = "이메일 형식으로 작성해주세요")
         private String email;
 
         @NotBlank
-        @Size(min = 4, max = 12, message = "4~12자 이내로 작성해주세요")
+        @Size(min = 8 ,message = "8자 이상으로 작성해주세요")
         private String password;
 
-        @NotNull
+        @NotNull(message = "생년월일을 입력해주세요")
         private Long birth;
 
         @NotNull(message = "성별을 선택해주세요")
