9cd2cb28670cc8d52fb1cea4426e5ac4833f2f9b
5solbin <dhrhfqls@naver.com>
2024-11-25 18:59:01 +0900

    validation 설정 (임의로)

--- src/main/java/savenow/backend/dto/user/UserReqDto.java
+++ src/main/java/savenow/backend/dto/user/UserReqDto.java
@@ -1,7 +1,6 @@
 package savenow.backend.dto.user;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,14 +13,23 @@ public class UserReqDto {
     @Setter
     // 조건은 임시로 정해놓은 상태
     public static class JoinReqDto {
-        @NotEmpty
+        // 회원 이름 : 한글, 영어 숫자 가능, 12자 이내
+        @NotBlank
+        @Pattern(regexp = "^[a-zA-Z가-힣0-9]{2,12}$" , message = "한글/영문/숫자 2~12자 이내로 작성해 주세요")
         private String username;
-        @NotEmpty
-        private String password;
-        @NotEmpty
+
+        @NotBlank
+        @Pattern(regexp = "^[a-zA-z0-9]{4,12}$", message = "한글/영문/숫자 4~12자 이내로 작성해주세요")
         private String email;
+
+        @NotBlank
+        @Size(min = 4, max = 12, message = "4~12자 이내로 작성해주세요")
+        private String password;
+
         @NotNull
         private Long birth;
+
+        @NotNull(message = "성별을 선택해주세요")
         private Gender gender;
 
         public User toEntity(BCryptPasswordEncoder passwordEncoder) {
